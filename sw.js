9f35ee31ec1d9326b87b36f9ac242e06f1ec0e7a
Yuka Mullen <quiwah@gmail.com>
2018-09-01 15:38:53 -0700

    Fixed typos

--- sw.js
+++ sw.js
@@ -1,23 +1,25 @@
 let cacheName = 'restaurant-cache-v1';
+let cache;
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(cacheName)
         .then(function(cache) {
-            const watchingUrl = [
+            cache.addAll([
                 '/',
                 '/index.html',
                 '/restaurant.html',
                 '/css/styles.css',
-                'js/main.js',
-                'js/restaurant_info.js',
-                'js/cache.js',
-                'js/dbhelper.js'
-            ];
-            return cache.addAll(watchingUrl);
+                '/js/main.js',
+                '/js/restaurant_info.js',
+                '/js/cache.js',
+                '/js/dbhelper.js'
+            ]);
         })
     );
 });
+
+let cacheName2 = 'restaurant-cache-v2';
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
@@ -26,18 +28,33 @@ self.addEventListener('fetch', function(event) {
             if (response) {
                 return response;
             } else {
+                //if there was no caches
                 return fetch(event.request)
                 .then(function(res) {
-                    return caches.open(cacheName)
+                    return caches.open(cacheName2)
                     .then(function(cache) {
                         cache.put(event.request.url, res.clone());
                         return res;
-        })
-    })
-    .catch(function() {
-        //for errors
-    });
-}
+                    })
+                })
+                .catch(function() {
+                    //for errors
+                });
+            }
         })
     );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then(function(keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName && key !== cacheName2) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
 });
